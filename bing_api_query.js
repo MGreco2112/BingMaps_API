25cc3988e10d5d2877f6b3eec3df9dff80e4461f
Matthew Greco <mgreco2112@tutanota.com>
2022-06-06 18:20:07 -0400

    coordinate search implemented

--- bing_api_query.js
+++ bing_api_query.js
@@ -1,4 +1,6 @@
-let bingMapsKey = localStorage.apiKey ? localStorage.apiKey : "";
+console.log(Api_Key);
+
+let bingMapsKey = localStorage.apiKey ? localStorage.apiKey : Api_Key;
 
 const geocode = () => {
     //create HTML Input element "input" with Submit button
@@ -21,7 +23,27 @@ const GeocodeCallback = (response) => {
         response.resourceSets.length > 0 &&
         response.resourceSets[0].resources) {
             
+        const results = response.resourceSets[0].resources;
 
+        console.log(results);
 
+        let table = [`<table><tr><td>Name</td><td>Latitude</td><td>Longitude</td></tr>`]
+
+        for (let i = 0; i < results.length; i++) {
+            table.push(`<tr><td> ${results[i].address.formattedAddress} </tr></td> <tr><td> ${results[i].bbox[0]} </tr></td> <tr><td> ${results[i].bbox[2]} </tr></td>`);
+        }
+
+        table.push('</table>');
+
+        output.innerHTML = table.join('');
+    } else {
+        output.innerHTML = "No results found.";
     }
+}
+
+const CallRestService = (request) => {
+    let script = document.createElement("script");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute("src", request);
+    document.body.appendChild(script);
 }
